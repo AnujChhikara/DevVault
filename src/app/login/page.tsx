17cdd39086413d175958a80942ac351d6110423e
AnujChhikara <anujchhikara777@gmail.com>
2024-03-15 17:49:32 +0530

    random

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import SignupFormDemo from '@/components/signupPage'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <SignupFormDemo/>
+    </div>
+  )
+}
+
+export default page
